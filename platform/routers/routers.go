fca842b6cc37ff25357df2d5c8042117106d944f
Shodiq <shodiq@tokopedia.com>
2020-01-06 12:58:22 +0700

    add middleware for http handler monitoring

--- platform/routers/routers.go
+++ platform/routers/routers.go
@@ -1,6 +1,9 @@
 package routers
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/savsgio/atreugo/v10"
 	"github.com/sirupsen/logrus"
 )
@@ -15,6 +18,12 @@ type Router struct {
 type routing struct {
 	host   string
 	domain string
+}
+
+type handlerInfo struct {
+	method string
+	path   string
+	time   time.Time
 }
 
 // Routers contains the functions of http handler to clean payloads and pass it the service
@@ -39,6 +48,28 @@ func (us *routing) Serve() {
 		Addr: us.host,
 	}
 	server := atreugo.New(config)
+
+	server.UseBefore(func(ctx *atreugo.RequestCtx) error {
+		ctx.SetUserValue("info", handlerInfo{
+			method: string(ctx.Method()),
+			path:   string(ctx.Path()),
+			time:   time.Now(),
+		})
+		return ctx.Next()
+	})
+
+	server.UseAfter(func(ctx *atreugo.RequestCtx) error {
+		info := ctx.UserValue("info").(handlerInfo)
+		timeHandler := time.Since(info.time)
+
+		// you can put datadog, prometheus or any monitoring platform
+		logrus.WithFields(logrus.Fields{
+			"path":   info.path,
+			"method": info.method,
+		}).Infoln(fmt.Sprintf("execution time: %v millisecond; %v microsecond", timeHandler.Milliseconds(), timeHandler.Microseconds()))
+
+		return ctx.Next()
+	})
 
 	for _, router := range handlers {
 		server.Path(router.Method, router.URL, router.Handler)
